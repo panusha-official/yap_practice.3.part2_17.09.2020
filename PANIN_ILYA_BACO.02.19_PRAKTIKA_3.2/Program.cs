9801f67c0ca27245191f9a88b41a1f736a427021
PaNuSHa <70700507+panusha-official@users.noreply.github.com>
2020-09-17 16:08:50 +0300

    Add files via upload

---
+++ PANIN_ILYA_BACO.02.19_PRAKTIKA_3.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace encod
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string fn = "file.txt";
+      StreamWriter sw = new StreamWriter(fn, false, Encoding.UTF7);
+      sw.WriteLine("Здравствуй, МИР!");
+      sw.Close();
+      StreamReader sr = new StreamReader(fn, Encoding.UTF7);
+      Console.WriteLine(sr.ReadToEnd());
+      sr.Close();
+    }
+  }
+}
